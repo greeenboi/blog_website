84a201c3e1bb9c99f8ce102e25baaa79ed934d35
greeenboi <suvan.gowrishanker.204@gmail.com>
2023-08-12 19:52:33 +0530

    setup sidebar ui

--- app/page.js
+++ app/page.js
@@ -10,14 +10,14 @@ export default function Home() {
         </section>
         <section className='flex flex-row h-max w-full'>
           <div className='grid grid-flow-row items-center justify-center lg:grid-flow-col h-fit flex-1 auto-cols-max gap-4 md:gap-6 lg:gap-10'>
-            <div className='flex flex-col p-6 border border-emerald-600 hover:border-emerald-800 m-2 items-center rounded-md'><h1>Title</h1>Hello <button>view post</button></div>
+            <div className='flex flex-col p-6 border border-emerald-600 hover:border-emerald-800 m-2 items-center rounded-md'><h1>Title</h1> <button>view post</button></div>
             <div className='flex flex-col p-6 border border-emerald-600 hover:border-emerald-800 m-2 items-center rounded-md'><h1>Title</h1>Hello <button>view post</button></div>
             <div className='flex flex-col p-6 border border-emerald-600 hover:border-emerald-800 m-2 items-center rounded-md'><h1>Title</h1>Hello <button>view post</button></div>
             <div className='flex flex-col p-6 border border-emerald-600 hover:border-emerald-800 m-2 items-center rounded-md'><h1>Title</h1>Hello <button>view post</button></div>
             <div className='flex flex-col p-6 border border-emerald-600 hover:border-emerald-800 m-2 items-center rounded-md'><h1>Title</h1>Hello <button>view post</button></div>
           </div>
           <div className='hidden md:flex flex-col scroll-m-1'>
-            <div className='flex flex-row gap-2 p-6 border border-emerald-600 hover:border-emerald-800 m-2 items-center rounded-md'><h1>Title</h1>Hello <button>view post</button></div>
+            <div className='flex flex-row gap-2 p-6 border border-emerald-600 hover:border-emerald-800 m-2 items-center rounded-md'><img width='128px' className='relative border border-teal-500 rounded-sm ' src="https://images.unsplash.com/photo-1541701494587-cb58502866ab?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"/> <h1 className='absolute ml-2'>Title</h1> <button>view post</button></div>
             <div className='flex flex-row gap-2 p-6 border border-emerald-600 hover:border-emerald-800 m-2 items-center rounded-md'><h1>Title</h1>Hello <button>view post</button></div>
             <div className='flex flex-row gap-2 p-6 border border-emerald-600 hover:border-emerald-800 m-2 items-center rounded-md'><h1>Title</h1>Hello <button>view post</button></div>
             <div className='flex flex-row gap-2 p-6 border border-emerald-600 hover:border-emerald-800 m-2 items-center rounded-md'><h1>Title</h1>Hello <button>view post</button></div>
