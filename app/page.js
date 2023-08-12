323d47d1546ab23bdee10a893108626f208497c7
greeenboi <suvan.gowrishanker.204@gmail.com>
2023-08-12 19:35:04 +0530

    first commit with semi complete landing page

---
+++ app/page.js
@@ -0,0 +1,30 @@
+
+import Navbar from '@/components/Navbar'
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <main className="flex min-h-screen flex-col items-center justify-between p-24 pt-12 ">
+        <section className="head-container font-sans">
+          <h1 className=''>THE BLOG</h1>
+        </section>
+        <section className='flex flex-row h-max w-full'>
+          <div className='grid grid-flow-row items-center justify-center md:grid-flow-col h-fit flex-1 auto-cols-max gap-4 md:gap-6 lg:gap-10'>
+            <div className='flex flex-col p-6 border border-emerald-600 hover:border-emerald-800 m-2 items-center rounded-md'><h1>Title</h1>Hello <button>view post</button></div>
+            <div className='flex flex-col p-6 border border-emerald-600 hover:border-emerald-800 m-2 items-center rounded-md'><h1>Title</h1>Hello <button>view post</button></div>
+            <div className='flex flex-col p-6 border border-emerald-600 hover:border-emerald-800 m-2 items-center rounded-md'><h1>Title</h1>Hello <button>view post</button></div>
+            <div className='flex flex-col p-6 border border-emerald-600 hover:border-emerald-800 m-2 items-center rounded-md'><h1>Title</h1>Hello <button>view post</button></div>
+            <div className='flex flex-col p-6 border border-emerald-600 hover:border-emerald-800 m-2 items-center rounded-md'><h1>Title</h1>Hello <button>view post</button></div>
+          </div>
+          <div className='hidden md:flex flex-col scroll-m-1'>
+            <div className='flex flex-row gap-2 p-6 border border-emerald-600 hover:border-emerald-800 m-2 items-center rounded-md'><h1>Title</h1>Hello <button>view post</button></div>
+            <div className='flex flex-row gap-2 p-6 border border-emerald-600 hover:border-emerald-800 m-2 items-center rounded-md'><h1>Title</h1>Hello <button>view post</button></div>
+            <div className='flex flex-row gap-2 p-6 border border-emerald-600 hover:border-emerald-800 m-2 items-center rounded-md'><h1>Title</h1>Hello <button>view post</button></div>
+            <div className='flex flex-row gap-2 p-6 border border-emerald-600 hover:border-emerald-800 m-2 items-center rounded-md'><h1>Title</h1>Hello <button>view post</button></div>
+            <div className='flex flex-row gap-2 p-6 border border-emerald-600 hover:border-emerald-800 m-2 items-center rounded-md'><h1>Title</h1>Hello <button>view post</button></div>          
+          </div>
+        </section>
+      </main>
+    </>
+  )
+}
---
+++ components/Navbar.jsx
@@ -0,0 +1,71 @@
+'use client'
+import React, { useState } from 'react';
+
+
+const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+     };
+
+  return (
+    <div className='flex flex-row md:flex-col md:w-screen md:h-28 w-auto h-auto items-center mt-2 pt-2 justify-evenly'>
+        <div className="flex flex-row justify-start ">
+        <img
+          className="dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
+          src="/logot.png"
+          alt="Next.js Logo"
+          width={280}
+          height={37}
+        />
+        <button className='hidden relative md:flex self-center ml-8 pl-6 pr-6 pt-1 pb-1 border border-emerald-300 rounded-md hover:bg-emerald-600 hover:opacity-75 md:cursor-pointer transition-opacity active:scale-105'>
+          login
+        </button>
+      </div>
+      <div className='md:hidden'>
+        <button
+          className='text-gray-600 hover:text-gray-900 focus:outline-none'
+          onClick={toggleMenu}
+        >
+          <svg
+            className='h-6 w-6 fill-gray-200'
+            viewBox='0 0 24 24'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            {menuOpen ? (
+              <path
+                fill-rule='evenodd'
+                clip-rule='evenodd'
+                d='M19 6H5a1 1 0 0 1 0-2h14a1 1 0 0 1 0 2zm0 5H5a1 1 0 0 1 0-2h14a1 1 0 0 1 0 2zm0 5H5a1 1 0 0 1 0-2h14a1 1 0 0 1 0 2z'
+              />
+            ) : (
+              <path
+                fill-rule='evenodd'
+                clip-rule='evenodd'
+                d='M4 6H20V5H4v1zm0 5h16v-1H4v1zm0 5h16v-1H4v1z'
+              />
+            )}
+          </svg>
+        </button>
+        {menuOpen && (            
+            <div className=' font-mono absolute right-0 opacity-80 mt-2 bg-gray-600 p-6 flex flex-col gap-2 rounded-md shadow-md '>
+                <p className='block cursor-pointer'>Populars</p>
+                <p className='block cursor-pointer'>Recent</p>
+                <p className='block cursor-pointer'>Announcements</p>
+                <p className='block cursor-pointer'>About us</p>
+                <button className=' p-2 border border-emerald-300 rounded-xl active:scale-105 ' >login</button>
+            </div>            
+        )}
+      </div>
+      <div className='md:flex flex-row justify-evenly md:gap-8 lg:gap-12 hidden'>
+        <p>Populars</p>
+        <p>Recent</p>
+        <p>Announcements</p>
+        <p>About us</p>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
