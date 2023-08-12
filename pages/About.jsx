2529d637e439233a6c88d70ff170277e869575e9
greeenboi <suvan.gowrishanker.204@gmail.com>
2023-08-12 23:05:28 +0530

    3rd bug fix for vercel

--- pages/About.jsx
+++ pages/About.jsx
@@ -25,7 +25,7 @@ const About = () => {
               
               Bloggerize, a platform that bridges the realm of curiosity with the world of coding and innovation, offering insights and inspiration to a diverse audience. <br/> <br/>
               The Creator, Suvan GS, is a dedicated second-year student at SRM University, driven by a profound passion for coding and its transformative potential.  <br/> <br/>
-              With a strong commitment to the pursuit of knowledge and personal growth, this blog serves as Suvan's medium for sharing the excitement of coding, intricate problem-solving, and the boundless creativity inherent in the field. <br/> <br/>
+              With a strong commitment to the pursuit of knowledge and personal growth, this blog serves as Suvan&apos;s medium for sharing the excitement of coding, intricate problem-solving, and the boundless creativity inherent in the field. <br/> <br/>
               Through meticulously crafted Posts, encompassing personal coding narratives, comprehensive tutorials, exploration of cutting-edge tech trends, and insightful observations, Suvan endeavors to provide value to learners across all stages of their journey. <br/> <br/>
               This blog is an invitation to embark on a voyage of collective learning and collaboration, where the shared experiences of transforming lines of code into impactful solutions foster growth and innovation. <br/> <br/>
               Join Suvan GS on this enlightening odyssey towards expanding horizons within the realm of coding and beyond.
